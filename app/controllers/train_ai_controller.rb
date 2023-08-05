8326bc732d93004d32a500901aea5322f77053ab
Orhan Sönmez <orhan@sonmez.xyz>
2023-08-05 14:43:29 +0200

    Implement adding additional data while training AI

--- app/controllers/train_ai_controller.rb
+++ app/controllers/train_ai_controller.rb
@@ -4,9 +4,6 @@ require 'dotenv'
 require 'ruby/openai'
 require 'csv'
 
-Dotenv.load()
-openai = OpenAI::Client.new(access_token: ENV['OPENAI_API_KEY'])
-
 class TrainAiController < ApplicationController
     def new
         @book = Book.new
@@ -18,9 +15,8 @@ class TrainAiController < ApplicationController
 
         if @book.save
             flash[:success] = "Book was successfully created. AI training is in progress!"
-            redirect_to url_for(controller: 'train_ai', action: 'show', id: @book)
         else
-            render 'new'
+            render json: { error: 'Book could not be created.' }, status: :unprocessable_entity
         end
     end
 
@@ -32,36 +28,49 @@ class TrainAiController < ApplicationController
         @book = Book.first
         
         if @book.present?
+            Dotenv.load()
+            openai = OpenAI::Client.new(access_token: ENV['OPENAI_API_KEY'])
+            tokenizer = Tokenizers.from_pretrained("gpt2")
+            
             pdf_path = Rails.root.join("training_data", "book.pdf")
 
             reader = PDF::Reader.new(pdf_path)
             page_count = 0
             
+            page_data = [["title", "content", "tokens"]]
             embeddings = []
 
             reader.pages.each do |page|
-                # Perform AI embeddings for the current page
+                # Create AI embeddings for the current page
                 response = openai.embeddings(
                     parameters: {
                         model: "text-search-curie-doc-001",
-                        input: text
+                        input: page.text
                     }
                 )
                 
                 # Increment the page count
                 page_count += 1
                 
-                # sleep(1)
-                # embedding = [123, 456, 789, 111, 222]
+                # Tokenize the page text and add the embeddings to an array
+                content = page.text.to_s.split().join(" ")
+                page_data << ["Page " + page_count.to_s, content, tokenizer.encode(content).tokens.length + 4]
                 embedding = response['data'][0]['embedding']
                 embeddings << {page: "Page " + page_count.to_s, embedding: embedding}
-                
 
                 # Send the progress to the client
                 ActionCable.server.broadcast("ai_progress_channel", { page: page_count, total_pages: reader.page_count })
             end
             
-            CSV.open("embeddings.csv", "w") do |csv|
+            # Save the page data to a CSV file
+            CSV.open("book_page_data.csv", "w") do |csv|
+                page_data.each do |obj|
+                    csv << obj
+                end
+            end
+            
+            # Save the embeddings to a CSV file
+            CSV.open("book_embeddings.csv", "w") do |csv|
                 csv << [:title].concat((0..4095).to_a)
                 embeddings.each do |obj|
                     csv << [obj[:page]].concat(obj[:embedding])
